01397845df1249bc592dc5152365445dd7c7dced
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-12-08 15:08:22 -0800

    create Home route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
