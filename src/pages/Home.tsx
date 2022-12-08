ed31bd76ebb6c76828a7b349a3547396e4d7adeb
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-12-08 15:21:22 -0800

    initialize header components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const LogoPlaceholder = styled.div``
+
+const NavigationPanel = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const NavigationRoute = styled.div``
+
+const CTAButton = styled.button``
 
 function Home() {
-  return <>Home</>
+  return (
+    <>
+      <Header>
+        <LogoPlaceholder>ODIS</LogoPlaceholder>
+        <NavigationPanel>
+          <NavigationRoute></NavigationRoute>
+          <NavigationRoute></NavigationRoute>
+          <NavigationRoute></NavigationRoute>
+          <NavigationRoute></NavigationRoute>
+        </NavigationPanel>
+        <CTAButton>Connect // Build with us</CTAButton>
+      </Header>
+    </>
+  )
 }
 
 export default Home
