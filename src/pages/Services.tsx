bdb4d04c657a406479fcba93d129ac9297058d54
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-12-09 15:50:26 -0800

    add Section components with client provided text in Services section

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import {SectionContainer, SectionTitle, SectionParagraph} from '../components/Section/Section'
 const PageWrapper = styled.div`
   max-width: 1600px;
   margin: 0 auto;
@@ -9,7 +9,24 @@ const PageWrapper = styled.div`
 `
 
 function Services() {
-  return <PageWrapper></PageWrapper>
+  return (
+    <PageWrapper>
+      <SectionContainer>
+        <SectionTitle text="Services" />
+        <SectionParagraph>
+          ODIS is an Advisory as a Service (AaaS) collective for Web3 companies and DAO’s. Through
+          our full suite of service offerings, we help founders bootstrap their community and
+          operations to scale on their own.
+        </SectionParagraph>
+        <SectionParagraph>
+          Our team consists of members with strong technical and non-technical backgrounds and
+          extensive experience at some of the top Web3 DAOs and companies such as Redacted Cartel,
+          Steel Perlot, Outlier Ventures, Covalent, YGG, GCR, New Order, 8it, and Krause House.
+        </SectionParagraph>
+        <SectionParagraph>Services Include:</SectionParagraph>
+      </SectionContainer>
+    </PageWrapper>
+  )
 }
 
 export default Services
