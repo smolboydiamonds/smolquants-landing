0e1b500429871e3e011922a9cd698fef9228730b
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-12-09 15:45:21 -0800

    add text paragraph content to About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -21,6 +21,12 @@ const Title = styled.div`
   margin: 16px 0;
 `
 
+const Paragraph = styled.div`
+  font-size: 16px;
+  font-family: 'Noto Sans JP', sans-serif;
+  font-weight: regular;
+  margin-top: 16px;
+`
 interface SectionTitleProps {
   text: string
 }
@@ -34,6 +40,27 @@ function About() {
     <PageWrapper>
       <SectionContainer>
         <SectionTitle text="About"></SectionTitle>
+        <Paragraph>
+          Odis is a collective of crypto-native developers, product managers, investors, and
+          community experts with past experiences working in both the large scale web2 software
+          industry as well as the web3 space. We are a founder-first firm that specializes in
+          helping you scale up your protocol to the next level by providing you engineering,
+          product, and community resources to help you gain wider mainstream adoption.
+        </Paragraph>
+        <Paragraph>
+          The amount of equity and/or tokens taken by other accelerators is disproportionately high
+          compared to the value they provide, negatively impacting the cap table and impairing
+          possible follow-on funding due to skewed tokenomics. After going through the long
+          onboarding process, accelerators throw companies into cohorts with a holistic approach,
+          which is an inefficient use of time and resources for founders.
+        </Paragraph>
+        <Paragraph>
+          We offer more founder-friendly terms than traditional accelerators and advisory services
+          while still aligning incentives for both parties. The onboarding process is less than two
+          weeks and a bespoke advisory program is developed for each client. Our core team has been
+          on both sides of the table and has the expertise to steer any Web3 startup in the right
+          direction.
+        </Paragraph>
       </SectionContainer>
     </PageWrapper>
   )
