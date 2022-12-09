33cce334a3829c1221a8fe5717ad6c5900841bf7
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-12-09 13:04:43 -0800

    Hero Header, Content, Description

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,10 +25,55 @@ const Copyright = styled.div`
 
 const SocialMediaContainer = styled.div``
 
+const HeroContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 850px;
+  font-family: 'Noto-Sans-JP', sans-serif;
+  font-weight: bold;
+  text-align: center;
+  margin: auto;
+`
+
+const HeroHeader = styled.div`
+  font-size: 64px;
+`
+
+const HeroSubheader = styled.div`
+  font-size: 24px;
+`
+
+const Line = styled.div`
+  height: 1px;
+  width: auto;
+  background: #fff;
+  margin: 18px;
+`
+
+const HeroDescription = styled.div`
+  font-size: 16px;
+`
+
+const CTAButton = styled.button`
+  width: 225px;
+  margin: 18px auto;
+`
+
 function Home() {
   return (
     <PageWrapper>
       <Header />
+      <HeroContent>
+        <HeroHeader>O D I S</HeroHeader>
+        <HeroSubheader>a web3 collective</HeroSubheader>
+        <Line />
+        <HeroDescription>
+          ODIS is an Advisory as a Service (AaaS) collective for Web3 companies and DAO’s. Through
+          our full suite of service offerings, we help founders bootstrap their community and
+          operations to scale on their own.
+        </HeroDescription>
+        <CTAButton>Connect // Build with us</CTAButton>
+      </HeroContent>
       <Footer>
         <Copyright>©2022 Odis. All Rights Reserved.</Copyright>
         <SocialMediaContainer></SocialMediaContainer>
