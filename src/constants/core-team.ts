ebbc3d312a0a8d3e0d649be9243dc34a706cd756
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-12-12 14:40:41 -0800

    add core team members constant file

---
+++ src/constants/core-team.ts
@@ -0,0 +1,47 @@
+export const CoreTeamUsers = [
+  {
+    name: 'Jack',
+    expertise: 'Business Development, DAO Ops, Fundraising, NFT, Token Distribution & Launch',
+    twitter_handle: 'hhjacks'
+  },
+  {
+    name: 'Mike',
+    expertise: 'Business Development, DAO Ops, Fundraising, Governance, Legal Structuring, NFT, Token Distribution & Launch',
+    twitter_handle: 'therealkazhang'
+  },
+  {
+    name: 'Jon',
+    expertise: 'Engineering, Product',
+    twitter_handle: 'jonathanjmak'
+  },
+  {
+    name: 'Sherv',
+    expertise: 'Business Development, Fundraising, NFT, Token Distribution & Launch',
+    twitter_handle: 'Sherv1n_eth'
+  },
+  {
+    name: 'iJackie',
+    expertise: 'Business Development, Community, NFT',
+    twitter_handle: 'iJackie_eth',
+  },
+  {
+    name: 'Niall',
+    expertise: 'Engineering, Product',
+    twitter_handle: 'niallyorke',
+  },
+  {
+    name: '0xElle',
+    expertise: 'Business Development, Community, DAO Ops, Fundraising, NFT',
+    twitter_handle: '0xElle',
+  },
+  {
+    name: 'Jacob',
+    expertise: 'Engineering, Product',
+    twiter_handle: 'jacobmakarsky'
+  },
+  {
+    name: 'Junney',
+    expertise: 'Business Development, Community, DAO Ops, Fundraising',
+    twitter_handle: 'nonfungie'
+  }
+]
