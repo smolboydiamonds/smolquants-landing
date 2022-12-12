c01ab5c1511a010d58ca3bf0fede31fb16cc303c
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-12-12 13:40:24 -0800

    create Accordion component to house service descriptions

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,81 @@
+import {useState} from 'react'
+import styled from 'styled-components'
+import {ChevronDown} from 'react-feather'
+import {Icon} from '../Icon/Icon'
+
+const AccordionWrapper = styled.div<{borderColor?: string}>``
+
+const AccordionText = styled.div<{color?: string}>`
+  font-weight: 700;
+  font-size: 14px;
+  margin: 12px 6px 12px 0px;
+  color: ${({color}) => color};
+`
+
+const ClickableDropdown = styled.div<{width?: string; clickableMargin?: string}>`
+  width: ${({width}) => (width ? width : '100%')};
+  margin: ${({clickableMargin}) => (clickableMargin ? clickableMargin : 'auto')};
+  display: flex;
+  cursor: pointer;
+`
+
+const Content = styled.div<{isOpen: boolean}>`
+  max-height: ${({isOpen}) => (isOpen ? '100vh' : '0vh')};
+  overflow: hidden;
+  display: block;
+  padding: ${({isOpen}) => (isOpen ? '16px 0' : 'auto')};
+`
+
+export const AccordionSelection = styled.div`
+  display: flex;
+  font-size: 12px;
+  padding: 0;
+`
+
+type AccordionProps = {
+  activeAccordionText: string | React.ReactNode
+  inactiveAccordionText: string | React.ReactNode
+  children?: React.ReactNode
+  activeColor?: string
+  inactiveColor?: string
+  width?: string
+  clickableMargin?: string
+}
+
+export const Accordion = ({
+  activeAccordionText,
+  inactiveAccordionText,
+  children,
+  activeColor,
+  inactiveColor,
+  width,
+  clickableMargin,
+}: AccordionProps) => {
+  const [isOpen, setOpen] = useState(false)
+
+  return (
+    <AccordionWrapper>
+      <ClickableDropdown
+        onClick={() => setOpen(!isOpen)}
+        clickableMargin={clickableMargin}
+        width={width}
+      >
+        {isOpen ? (
+          <AccordionText color={activeColor}>{activeAccordionText}</AccordionText>
+        ) : (
+          <AccordionText color={inactiveColor}>{inactiveAccordionText}</AccordionText>
+        )}
+        <Icon
+          size={16}
+          clickable={true}
+          margin={'auto 0 auto auto'}
+          color={isOpen ? activeColor : inactiveColor}
+          transform={isOpen ? 'rotate(180deg)' : ''}
+        >
+          <ChevronDown height={16} width={16} />
+        </Icon>
+      </ClickableDropdown>
+      <Content isOpen={isOpen}>{children}</Content>
+    </AccordionWrapper>
+  )
+}
