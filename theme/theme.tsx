e028b99f3169531529f72d692fedca1494533755
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-12-13 10:03:35 -0800

    init theme file, add media width templates to be used in component creation

---
+++ theme/theme.tsx
@@ -0,0 +1,29 @@
+import styled, {
+  css,
+  DefaultTheme,
+  ThemeProvider as StyledComponentsThemeProvider,
+} from 'styled-components'
+
+export const MEDIA_WIDTHS = {
+  minExtraSmall: 576,
+  minSmall: 768,
+  minMedium: 992,
+  minLarge: 1400,
+}
+
+const mediaWidthTemplates: {[width in keyof typeof MEDIA_WIDTHS]: typeof css} = Object.keys(
+  MEDIA_WIDTHS,
+).reduce((accumulator, size) => {
+  ;(accumulator as any)[size] = (a: any, b: any, c: any) => css`
+    @media (min-width: ${(MEDIA_WIDTHS as any)[size]}px) {
+      ${css(a, b, c)}
+    }
+  `
+  return accumulator
+}, {}) as any
+
+export function theme(): DefaultTheme {
+  return {
+    mediaWidth: mediaWidthTemplates,
+  }
+}
