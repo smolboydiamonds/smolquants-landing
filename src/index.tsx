93ae9a367780a4363f9f136879d804d2446526eb
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-12-13 11:22:57 -0800

    move theme folder into /src

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ThemeProvider from '../theme/theme'
+import ThemeProvider from './theme/theme'
 import {HashRouter} from 'react-router-dom'
 import './index.css'
 import App from './App'
