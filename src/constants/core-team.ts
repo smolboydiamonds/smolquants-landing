8ac81a5f16ea2880fe74360ce907f98521d0d199
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-01-30 18:54:50 -0800

    clean

--- src/constants/core-team.ts
+++ src/constants/core-team.ts
@@ -1,13 +1,5 @@
 import { UserProfileProps } from "../components/UserProfile/UserProfile"
 import Jack_PFP from '../assets/images/jack-pfp.png'
-import Jackie_PFP from '../assets/images/jackie-pfp.png'
-import Jacob_PFP from '../assets/images/jacob-pfp.jpg'
-import Junney_PFP from '../assets/images/junney-pfp.png'
-import Niall_PFP from '../assets/images/niall-pfp.png'
-import Mike_PFP from '../assets/images/mike-pfp.png'
-import Chris_PFP from '../assets/images/chris-pfp.png'
-import JMak_PFP from '../assets/images/jmak_pfp.jpeg'
-import Sherv_PFP from '../assets/images/sherv_pfp.png'
 
 export const CoreTeamUsers: UserProfileProps[] = [
   {
@@ -18,68 +10,4 @@ export const CoreTeamUsers: UserProfileProps[] = [
     medium_handle: undefined,
     pfp_src: Jack_PFP,
   },
-  {
-    name: 'Mike',
-    expertise: 'Business Development, DAO Ops, Fundraising, Governance, Legal Structuring, NFT, Token Distribution & Launch',
-    twitter_handle: 'therealkazhang',
-    github_handle: undefined,
-    medium_handle: undefined,
-    pfp_src: Mike_PFP
-  },
-  {
-    name: 'Jon',
-    expertise: 'Engineering, Product',
-    twitter_handle: 'jonathanjmak',
-    github_handle: undefined,
-    medium_handle: undefined,
-    pfp_src: JMak_PFP
-  },
-  {
-    name: 'Sherv',
-    expertise: 'Business Development, Fundraising, NFT, Token Distribution & Launch',
-    twitter_handle: 'Sherv1n_eth',
-    github_handle: undefined,
-    medium_handle: undefined,
-    pfp_src: Sherv_PFP
-  },
-  {
-    name: 'iJackie',
-    expertise: 'Business Development, Community, NFT',
-    twitter_handle: 'iJackie_eth',
-    github_handle: undefined,
-    medium_handle: undefined,
-    pfp_src: Jackie_PFP
-  },
-  {
-    name: 'Niall',
-    expertise: 'Product, Marketing, Community',
-    twitter_handle: 'niallyorke',
-    github_handle: undefined,
-    medium_handle: undefined,
-    pfp_src: Niall_PFP
-  },
-  {
-    name: 'Jacob',
-    expertise: 'Engineering, Product',
-    twitter_handle: 'jacobmakarsky',
-    github_handle: undefined,
-    medium_handle: undefined,
-    pfp_src: Jacob_PFP
-  },
-  {
-    name: 'Junney',
-    expertise: 'Business Development, Community, DAO Ops, Fundraising',
-    twitter_handle: 'nonfungie',
-    github_handle: undefined,
-    medium_handle: undefined,
-    pfp_src: Junney_PFP
-  },
-  {
-    name: 'Chris',
-    expertise: 'Community, Business Development, Events, NFT',
-    twitter_handle: undefined,
-    github_handle: undefined,
-    medium_handle: undefined,
-    pfp_src: Chris_PFP
-  }
 ]
